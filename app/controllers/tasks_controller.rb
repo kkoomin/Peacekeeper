fc456c711b8d7735d603c55d60b3755d762653d2
kkoomin <minha.koo.9@gmail.com>
2019-02-11 10:45:45 +0000

    created some forms for tasks and users

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,39 @@
 class TasksController < ApplicationController
+    before_action :find_user, only: [:show, :edit, :destroy]
+
+    def index
+        @tasks = Task.all
+    end
+
+    def show
+    end
+
+    def new
+        @task = Task.new
+        @status_list = ["uncompleted", "claimed", "completed"]
+    end
+
+    def create
+        @task = Task.create(task_params)
+        redirect_to task_path(@task)
+    end
+
+    def edit
+        @task.update(task_params)
+    end
+
+    def destroy
+        @task.destroy
+        
+    end
+
+    private
+
+    def task_params
+        params.require(:task).permit(:name, :description, :status, :deadline, :pinned, :user_id)
+    end
+
+    def find_task
+        @task = task.find(params[:id])
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,45 @@
 class UsersController < ApplicationController
+    before_action :find_user, only: [:show, :edit, :destroy]
+
+    def index
+        @users = User.all
+    end
+
+    def show
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.create(user_params)
+        redirect_to user_path(@user)
+    end
+
+    def edit
+        @user.update(user_params)
+    end
+
+    def destroy
+        @user.destroy
+        
+    end
+
+    def upload
+        uploaded_io = params[:user][:profile_pic_url]
+        File.open(Rails.root.join('public', 'uploads', uploaded_io.original_filename), 'wb') do |file|
+        file.write(uploaded_io.read)
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :password, :nickname, :profile_pic_url)
+    end
+
+    def find_user
+        @user = User.find(params[:id])
+    end
 end
