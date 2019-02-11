47cf293650467b89f9626e4725958ad20142c679
jchaselubitz <jchaselubitz@gmail.com>
2019-02-11 10:00:49 +0000

    added basic models and created views files

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,6 @@
 class Task < ApplicationRecord
+    has_many :comments
+    belongs_to :user
+     # validation
+     
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+    has_many :tasks
+    has_many :comments
+    # validation
+
+
+    def claimed
+    end
+
+    def completed
+    end
+
 end
