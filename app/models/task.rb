4b6c9d1db2f83a0219336fbffd159416e7b36448
kkoomin <minha.koo.9@gmail.com>
2019-02-11 18:09:02 +0000

    task status_description

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,5 +2,7 @@ class Task < ApplicationRecord
     has_many :comments
     belongs_to :user
      # validation
-     
+    def status_description
+        self.status == true ? "Complete" : "Incomplete"
+    end
 end
