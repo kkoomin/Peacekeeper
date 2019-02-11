54e560de77658e12c3287ae454e8ca92d22ffe38
jchaselubitz <jchaselubitz@gmail.com>
2019-02-11 14:07:37 +0000

    added html for checklist

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :comments
   resources :users
   resources :tasks
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
