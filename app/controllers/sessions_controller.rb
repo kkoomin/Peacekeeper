898f40ed724b12df542bb0a942e6f2b42206fff8
kkoomin <minha.koo.9@gmail.com>
2019-02-12 14:33:29 +0000

    try to merge

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,18 +5,16 @@ class SessionsController < ApplicationController
 
     def create
         @user = User.find_by(email: params[:user][:email])
-
         @user = @user.try(:authenticate, params[:user][:password])
-    
         return redirect_to login_path unless @user
-    
         session[:user_id] = @user.id
-    
         redirect_to user_path(@user)
+        
     end
 
     def destroy
         session.delete :user_id
+        #or "session[:user_id] = nil"
         redirect_to login_path
     end
 
